35af489a1bb20f2465ac60dfefe533af7dab0200
Miltiade <vero@tutamail.com>
2026-02-24 07:54:19 +0200

    For "issue" & "comment" features: setup serializers, viewsets & routes

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
-from .views import ProjectViewSet, ContributorViewSet
+from .views import ProjectViewSet, ContributorViewSet, IssueViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet)
 router.register(r'contributors', ContributorViewSet)
+router.register(r'issues', IssueViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = router.urls
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
-from .models import Project, Contributor
-from .serializers import ProjectSerializer, ContributorSerializer
 from rest_framework.permissions import IsAuthenticated
+from .models import Project, Contributor, Issue, Comment
+from .serializers import ProjectSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
@@ -11,4 +12,14 @@ class ProjectViewSet(viewsets.ModelViewSet):
 class ContributorViewSet(viewsets.ModelViewSet):
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
+    permission_classes = [IsAuthenticated]
+
+class IssueViewSet(viewsets.ModelViewSet):
+    queryset = Issue.objects.all()
+    serializer_class = IssueSerializer
+    permission_classes = [IsAuthenticated]
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
