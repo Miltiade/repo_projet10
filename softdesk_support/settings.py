3e857ccb1e4cfe59d43fde874e01263c5dc0a588
Miltiade <vero@tutamail.com>
2026-02-24 07:42:09 +0200

    externalized secret key

--- softdesk_support/settings.py
+++ softdesk_support/settings.py
@@ -10,17 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!f7%jhj=ce5)xe)15(m!vih3u^v*$y%x5*nnq1fugjabd&^64m'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "clé_par_defaut_pour_dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
