dfec2617bfbcbc371cc1427ba3721006bf96e669
Miltiade <vero@tutamail.com>
2026-02-25 12:17:56 +0200

    updated serializers

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Project, Contributor, Issue, Comment
 
 class ProjectSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Project
         fields = '__all__'
@@ -12,11 +14,25 @@ class ContributorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class IssueSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
+    assignee = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        required=False,
+        allow_null=True
+    )
+    status = serializers.ChoiceField(choices=Issue.STATUS_CHOICES, default='todo')
+    priority = serializers.ChoiceField(choices=Issue.PRIORITY_CHOICES, default='medium')
+
     class Meta:
         model = Issue
         fields = '__all__'
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    issue = serializers.PrimaryKeyRelatedField(queryset=Issue.objects.all())
+    uuid = serializers.UUIDField(read_only=True)
+
     class Meta:
         model = Comment
         fields = '__all__'
--- users/serializers.py
+++ users/serializers.py
@@ -6,8 +6,5 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         # Liste des champs à exposer / manipuler via l’API
         fields = [
-            'id', 'username', 'email', 'first_name', 'last_name',
-            'role', 'can_be_contacted', 'can_data_be_shared', 'date_of_birth',
+            'id', 'username', 'role', 'can_be_contacted', 'can_data_be_shared', 'date_of_birth',
         ]
-        # On peut en exclure certains via `exclude` si besoin
-        # Exclure le mot de passe et autres champs sensibles
