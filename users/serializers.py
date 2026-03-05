4bdbe4a334aaa882e7b118a29c286b11061b3cb0
Miltiade <vero@tutamail.com>
2026-03-05 11:25:05 +0200

    debugged: user must be <16 y.o.

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from django.utils import timezone
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'username', 'role', 'can_be_contacted', 'can_data_be_shared', 'date_of_birth',
         ]
+
+    def validate_date_of_birth(self, value):
+        if value is None:
+            raise serializers.ValidationError("Renseigner la date de naissance est obligatoire.")
+        today = timezone.now().date()
+        age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+        if age < 15:
+            raise serializers.ValidationError("Inscription interdite : l'utilisateur doit avoir au moins 15 ans pour créer un compte.")
+        return value
