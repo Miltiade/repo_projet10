0c9fd98292396b5a1a08574e6bee3a3b60ef7054
Miltiade <vero@tutamail.com>
2026-03-10 11:30:19 +0200

    debugged: user can only access their personal data (not others')

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets, generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, BasePermission
 from .models import User
 from .serializers import UserSerializer
 
+class IsOwner(BasePermission):
+    """
+    Permission qui permet l’accès uniquement à l’objet appartenant à l’utilisateur connecté.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
 
 class UserCreateView(generics.CreateAPIView):
     """
@@ -17,6 +23,9 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     Vue pour opérations CRUD sur utilisateurs existants, accessible uniquement aux utilisateurs authentifiés.
     """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer # Identifie le projet lié à l'objet
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        # L’utilisateur ne reçoit en réponse que ses propres données
+        return User.objects.filter(pk=self.request.user.pk)
