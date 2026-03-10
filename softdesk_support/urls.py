cc926353e3c45c69d98a249f11c18cb8034be632
Miltiade <vero@tutamail.com>
2026-03-10 11:08:23 +0200

    created: feature "register new user"

--- softdesk_support/urls.py
+++ softdesk_support/urls.py
@@ -23,7 +23,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),# ex: accès API via /api/users/
+    path('api/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),# pour obtenir le token
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),# pour rafraîchir le token
     path('api/', include('projects.urls')),
--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated  # pour sécuriser l'accès
 
-class UserViewSet(viewsets.ModelViewSet):
+
+class UserCreateView(generics.CreateAPIView):
+    """
+    Vue dédiée à la création d’utilisateur (inscription), accessible sans authentification.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]  # à ajuster selon besoins
+    permission_classes = [AllowAny]
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Vue pour opérations CRUD sur utilisateurs existants, accessible uniquement aux utilisateurs authentifiés.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
