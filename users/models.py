cb623240b192c6898494412c25b557d520cace9c
Miltiade <vero@tutamail.com>
2026-03-10 11:56:32 +0200

    debugged: collecting user consent (GDPR)

--- users/models.py
+++ users/models.py
@@ -19,9 +19,9 @@ class User(AbstractUser):
     def is_admin(self):
         return self.role == 'admin'
     
-    can_be_contacted = models.BooleanField(default=True)
-
-    can_data_be_shared = models.BooleanField(default=False)
+    can_be_contacted = models.BooleanField(default=None, null=True, blank=True)
+    
+    can_data_be_shared = models.BooleanField(default=None, null=True, blank=True)
 
     date_of_birth = models.DateField(null=True, blank=True)
 
--- users/serializers.py
+++ users/serializers.py
@@ -3,16 +3,26 @@ from .models import User
 from django.utils import timezone
 
 class UserSerializer(serializers.ModelSerializer):
+    can_be_contacted = serializers.BooleanField(
+        required=True,
+        error_messages={'required': 'Consentez-vous à être contacté ? Réponse obligatoire.'}
+    )
+    can_data_be_shared = serializers.BooleanField(
+        required=True,
+        error_messages={'required': 'Consentez-vous au partage de vos données ? Réponse obligatoire.'}
+    )
+    date_of_birth = serializers.DateField(
+        required=True,
+        error_messages={'required': 'Renseigner la date de naissance est obligatoire.'}
+    )
+
     class Meta:
         model = User
-        # Liste des champs à exposer / manipuler via l’API
         fields = [
             'id', 'username', 'role', 'can_be_contacted', 'can_data_be_shared', 'date_of_birth',
         ]
 
     def validate_date_of_birth(self, value):
-        if value is None:
-            raise serializers.ValidationError("Renseigner la date de naissance est obligatoire.")
         today = timezone.now().date()
         age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
         if age < 15:
